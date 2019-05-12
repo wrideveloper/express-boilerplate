a23f615cb12718aacb5ec980ce9cca941c062fbc
mnindrazaka <mnindrazaka@gmail.com>
2019-05-13 04:57:45 +0700

    express crud

---
+++ express-boilerplate.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 13, 2019 at 04:56 AM
+-- Server version: 5.7.26-0ubuntu0.18.04.1
+-- PHP Version: 7.2.17-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `express-boilerplate`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `pages` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `title`, `pages`, `category_id`) VALUES
+(3, 'GraphQL Introduction', 200, 1),
+(4, 'Microservices Introduction', 200, 1),
+(5, 'Unity Master', 300, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'web development'),
+(2, 'game development'),
+(4, 'internet of things');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.js
+++ index.js
@@ -1,8 +1,141 @@
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
+const mysql = require('mysql')
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'express-boilerplate'
+})
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+app.use(methodOverride('method'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/book', function(req, res) {
+  con.query(
+    `SELECT
+      book.id, 
+      book.title, 
+      book.pages, 
+      category.name AS category_name 
+      FROM book JOIN category ON category.id = book.category_id`,
+    function(err, result) {
+      res.render('book/index', { books: result })
+    }
+  )
+})
+
+app.get('/book/create', function(req, res) {
+  con.query('SELECT * From category', function(err, result) {
+    res.render('book/create', { categories: result })
+  })
+})
+
+app.post('/book', function(req, res) {
+  const { title, pages, category_id } = req.body
+  con.query(
+    `INSERT INTO book SET title='${title}', pages=${pages}, category_id=${category_id}`,
+    function(err, result) {
+      res.redirect('/book')
+    }
+  )
+})
+
+app.get('/book/:id/edit', function(req, res) {
+  con.query(
+    `SELECT 
+      book.id,
+      book.title, 
+      book.pages,
+      book.category_id
+      FROM book WHERE id=${req.params.id}`,
+    function(err, books) {
+      con.query(
+        `SELECT 
+          category.id, 
+          category.name 
+          From category`,
+        function(err, categories) {
+          res.render('book/edit', { book: books[0], categories: categories })
+        }
+      )
+    }
+  )
+})
+
+app.put('/book/:id', function(req, res) {
+  const { title, pages, category_id } = req.body
+  con.query(
+    `UPDATE book SET 
+      title='${title}', 
+      pages=${pages},
+      category_id=${category_id}
+      WHERE id=${req.params.id}`,
+    function(err, result) {
+      if (err) throw err
+      res.redirect('/book')
+    }
+  )
+})
+
+app.delete('/book/:id', function(req, res) {
+  con.query(`DELETE FROM book WHERE id=${req.params.id}`, function(
+    err,
+    result
+  ) {
+    res.redirect('/book')
+  })
+})
+
+app.get('/category', function(req, res) {
+  con.query(`SELECT * FROM category`, function(err, result) {
+    res.render('category/index', { categories: result })
+  })
+})
+
+app.get('/category/create', function(req, res) {
+  res.render('category/create')
+})
+
+app.post('/category', function(req, res) {
+  const { name } = req.body
+  con.query(`INSERT INTO category SET name='${name}'`, function(err, result) {
+    res.redirect('/category')
+  })
+})
+
+app.get('/category/:id/edit', function(req, res) {
+  con.query(`SELECT * FROM category WHERE id=${req.params.id}`, function(
+    err,
+    result
+  ) {
+    res.render('category/edit', { category: result[0] })
+  })
+})
+
+app.put('/category/:id', function(req, res) {
+  const { name } = req.body
+  con.query(
+    `UPDATE category SET name='${name}' WHERE id=${req.params.id}`,
+    function(err, result) {
+      res.redirect('/category')
+    }
+  )
+})
+
+app.delete('/category/:id', function(req, res) {
+  con.query(`DELETE FROM category WHERE id=${req.params.id}`, function(
+    err,
+    result
+  ) {
+    res.redirect('/category')
+  })
 })
 
 app.listen(3000, function() {
